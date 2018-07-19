aab6d418ebb6fb29bb1c5061a89f0286e1357493
Efe <32391199+efetoboreakpoguma@users.noreply.github.com>
2018-07-19 09:38:07 -0400

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from random import randint
 print(f'''
 {'Game Rules':*^110}
 If a player's guess is less than 1 or greater than 100, say "OUT OF BOUNDS"
+It doesn't count the tries that are "OUT OF BOUNDS!"
 On a player's first turn, if their guess is within 10 of the number, return "WARM!"
 If it is further than 10 away from the number, return "COLD!"
 On all subsequent turns, if a guess is closer to the number than the previous guess return "WARMER!"
